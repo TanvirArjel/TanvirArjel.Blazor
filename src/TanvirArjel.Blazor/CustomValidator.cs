4975c927a0505195e72589840613bbaf5936da46
Tanvir Ahmad Arjel <tanvirarjel@outlook.com>
2021-06-14 23:50:32 +0600

    CustomValidator has been added.

---
+++ src/TanvirArjel.Blazor/CustomValidator.cs
@@ -0,0 +1,128 @@
+﻿// <copyright file="CustomValidator.cs" company="TanvirArjel">
+// Copyright (c) TanvirArjel. All rights reserved.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace TanvirArjel.Blazor
+{
+    /// <summary>
+    /// Contains methods for adding custom validation message to Blazor FormContext.
+    /// </summary>
+    public class CustomValidator : ComponentBase
+    {
+        private ValidationMessageStore _messageStore;
+
+        [CascadingParameter]
+        private EditContext CurrentEditContext { get; set; }
+
+        /// <summary>
+        /// Add a generic error message to the current form context.
+        /// </summary>
+        /// <param name="errorMessage">The error message to be added.</param>
+        public void AddError(string errorMessage)
+        {
+            _messageStore.Add(CurrentEditContext.Field(string.Empty), errorMessage);
+        }
+
+        /// <summary>
+        /// Add a generic error message to the current form context and display.
+        /// </summary>
+        /// <param name="errorMessage">The error message to be added and displayed.</param>
+        public void AddAndDisplayError(string errorMessage)
+        {
+            _messageStore.Add(CurrentEditContext.Field(string.Empty), errorMessage);
+            CurrentEditContext.NotifyValidationStateChanged();
+        }
+
+        /// <summary>
+        /// Add a key specified error message to the current form context.
+        /// </summary>
+        /// <param name="key">The key of the error message.</param>
+        /// <param name="errorMessage">The message to be added.</param>
+        public void AddError(string key, string errorMessage)
+        {
+            _messageStore.Add(CurrentEditContext.Field(key), errorMessage);
+        }
+
+        /// <summary>
+        /// Add a key specified error message to the current form context and display.
+        /// </summary>
+        /// <param name="key">The key of the error message.</param>
+        /// <param name="errorMessage">The message to be added and displayed.</param>
+        public void AddAndDisplayError(string key, string errorMessage)
+        {
+            _messageStore.Add(CurrentEditContext.Field(key), errorMessage);
+            CurrentEditContext.NotifyValidationStateChanged();
+        }
+
+        /// <summary>
+        /// Add a collection of error messages to the current context and display.
+        /// </summary>
+        /// <param name="errors">The erros to be added and displayed.</param>
+        public void AddAndDiplayErrors(IDictionary<string, List<string>> errors)
+        {
+            AddErrors(errors);
+            CurrentEditContext.NotifyValidationStateChanged();
+        }
+
+        /// <summary>
+        /// Add a collection of error messages to the current context.
+        /// </summary>
+        /// <param name="errors">The erros to be added.</param>
+        public void AddErrors(IDictionary<string, List<string>> errors)
+        {
+            if (errors == null)
+            {
+                return;
+            }
+
+            foreach (KeyValuePair<string, List<string>> err in errors)
+            {
+                _messageStore.Add(CurrentEditContext.Field(err.Key), err.Value);
+            }
+        }
+
+        /// <summary>
+        /// Display all the errors of the current context.
+        /// </summary>
+        public void DisplayErrors()
+        {
+            CurrentEditContext.NotifyValidationStateChanged();
+        }
+
+        /// <summary>
+        /// Clear all the errors of the current context.
+        /// </summary>
+        public void ClearErrors()
+        {
+            _messageStore.Clear();
+            CurrentEditContext.NotifyValidationStateChanged();
+        }
+
+        /// <summary>
+        /// Called when the CustomValidator component is initialized.
+        /// </summary>
+        protected override void OnInitialized()
+        {
+            if (CurrentEditContext == null)
+            {
+                throw new InvalidOperationException(
+                    $"{nameof(CustomValidator)} requires a cascading " +
+                    $"parameter of type {nameof(EditContext)}. " +
+                    $"For example, you can use {nameof(CustomValidator)} " +
+                    $"inside an {nameof(EditForm)}.");
+            }
+
+            _messageStore = new ValidationMessageStore(CurrentEditContext);
+
+            CurrentEditContext.OnValidationRequested += (s, e) =>
+                _messageStore.Clear();
+            CurrentEditContext.OnFieldChanged += (s, e) =>
+                _messageStore.Clear(e.FieldIdentifier);
+        }
+    }
+}
---
+++ src/TanvirArjel.Blazor/CustomValidatorExtensions.cs
@@ -0,0 +1,100 @@
+﻿// <copyright file="CustomValidatorExtensions.cs" company="TanvirArjel">
+// Copyright (c) TanvirArjel. All rights reserved.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace TanvirArjel.Blazor
+{
+    /// <summary>
+    /// Contains extension methods on <see cref="CustomValidator"/>.
+    /// </summary>
+    public static class CustomValidatorExtensions
+    {
+        /// <summary>
+        /// Add and diplay errors from the <see cref="HttpResponseMessage"/> object.
+        /// </summary>
+        /// <param name="customValidator">The <see cref="CustomValidator"/> object.</param>
+        /// <param name="httpResponseMessage">The <see cref="HttpResponseMessage"/> object.</param>
+        /// <returns>Returns <see cref="Task"/>.</returns>
+        /// <exception cref="ArgumentNullException">Throws if <paramref name="customValidator"/> is <see langword="null"/>.</exception>
+        /// <exception cref="ArgumentNullException">Throws if <paramref name="httpResponseMessage"/> is <see langword="null"/>.</exception>
+        public static async Task AddErrorsAndDisplayAsync(this CustomValidator customValidator, HttpResponseMessage httpResponseMessage)
+        {
+            await AddErrorsAsync(customValidator, httpResponseMessage).ConfigureAwait(false);
+            customValidator.DisplayErrors();
+        }
+
+        /// <summary>
+        /// Add errors from the <see cref="HttpResponseMessage"/> object to the current form context.
+        /// </summary>
+        /// <param name="customValidator">The <see cref="CustomValidator"/> object.</param>
+        /// <param name="httpResponseMessage">The <see cref="HttpResponseMessage"/> object.</param>
+        /// <returns>Returns <see cref="Task"/>.</returns>
+        /// <exception cref="ArgumentNullException">Throws if <paramref name="customValidator"/> is <see langword="null"/>.</exception>
+        /// <exception cref="ArgumentNullException">Throws if <paramref name="httpResponseMessage"/> is <see langword="null"/>.</exception>
+        public static async Task AddErrorsAsync(this CustomValidator customValidator, HttpResponseMessage httpResponseMessage)
+        {
+            if (customValidator == null)
+            {
+                throw new ArgumentNullException(nameof(customValidator));
+            }
+
+            if (httpResponseMessage == null)
+            {
+                throw new ArgumentNullException(nameof(httpResponseMessage));
+            }
+
+            if (httpResponseMessage.IsSuccessStatusCode == false)
+            {
+                Dictionary<string, List<string>> errors = new Dictionary<string, List<string>>();
+
+                if ((int)httpResponseMessage.StatusCode == 400)
+                {
+                    JsonSerializerOptions jsonSerializerOptions = new JsonSerializerOptions()
+                    {
+                        PropertyNameCaseInsensitive = true
+                    };
+
+                    string responseString = await httpResponseMessage.Content.ReadAsStringAsync().ConfigureAwait(false);
+                    try
+                    {
+                        Dictionary<string, List<string>> modelErrors = JsonSerializer.Deserialize<Dictionary<string, List<string>>>(responseString, jsonSerializerOptions);
+                        if (modelErrors.Any())
+                        {
+                            foreach (KeyValuePair<string, List<string>> error in modelErrors)
+                            {
+                                string fieldName = error.Key;
+                                List<string> errorMessages = error.Value;
+                                errors.Add(fieldName, errorMessages);
+                            }
+                        }
+                        else
+                        {
+                            errors.Add(string.Empty, new List<string>() { "Invalid request. One or more validations failed." });
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        errors.Add(string.Empty, new List<string>() { "Invalid request. One or more validations failed." });
+                    }
+                }
+                else if ((int)httpResponseMessage.StatusCode == 401)
+                {
+                    errors.Add(string.Empty, new List<string>() { AppErrorMessage.UnAuthorizedErrorMessage });
+                }
+                else
+                {
+                    errors.Add(string.Empty, new List<string> { AppErrorMessage.ServerErrorMessage });
+                }
+
+                customValidator.AddErrors(errors);
+            }
+        }
+    }
+}
